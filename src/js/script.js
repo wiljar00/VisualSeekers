97195070937b459f95ddcb8fb32618b3884c49e3
pratyushasai <kpratyushasai@gmail.com>
2018-11-13 23:18:52 -0800

    p 13 nov 1

---
+++ src/js/script.js
@@ -0,0 +1,59 @@
+d3.csv("data/ObeseAdults.csv").then(obeseData => {
+    obeseList=FormatData(obeseData);
+    console.log(obeseList,"obeseList");
+});
+
+d3.csv("data/vegetableConsumption.csv").then(vegConsumptiondata => {
+    vegConsumptionList=FormatData(vegConsumptiondata);
+    console.log(vegConsumptionList,"vegConsumptionList");
+});
+
+d3.csv("data/fruitConsumption.csv").then(fruitConsumptiondata => {
+   fruitConsumptionList=FormatData(fruitConsumptiondata);
+    console.log(fruitConsumptionList,"fruitConsumptionList");
+});
+
+d3.csv("data/life-expectancy.csv").then(lifeExpectancydata => {
+   lifeExpectancyList=FormatData(lifeExpectancydata);
+    console.log(lifeExpectancyList,"lifeExpectancyList");
+});
+
+d3.csv("data/share_of_females.csv").then(shareOfFemales => {
+   shareOfFemalesList=FormatData(shareOfFemales);
+    console.log(shareOfFemalesList,"shareOfFemales");
+});
+
+d3.csv("data/share_of_males.csv").then(shareOfMales => {
+   shareOfMalesList=FormatData(shareOfMales);
+    console.log(shareOfMalesList,"shareOfMales");
+});
+
+d3.json("data/world.json")
+    .then(function(world) {
+        console.log(world,"world");
+    });
+
+function FormatData(data)
+{
+    var List=[];
+  countryName="";
+  j=0;
+  for(i=0;i<data.length;i++){
+    if((i==0 || data[i].Entity!=countryName)&&data[i].Code!=""&&data[i].Indicator!=""&&(data[i].Year>=1976&&data[i].Year<=2013))
+    {
+       var country={};
+        country.Name=data[i].Entity;
+        country.Code=data[i].Code;
+        country.Year=[];
+        countryName=data[i].Entity;
+        List.push(country);
+        j=j+1;
+    }
+    else if(data[i].Code!=""&&data[i].Indicator!=""&&(data[i].Year>=1976&&data[i].Year<=2013)){
+        List[j-1].Year.push({Year:data[i].Year,Indicator:data[i].Indicator})
+    }
+}
+  return List;
+}
+
+
